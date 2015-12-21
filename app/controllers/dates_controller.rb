3d1bdf6f89de0f1185ca80643a1e6299713f5841
Scott Robertson <scottymeuk@gmail.com>
2015-12-21 23:22:48 +0000

    Adding stuff

---
+++ app/controllers/dates_controller.rb
@@ -0,0 +1,15 @@
+class DatesController < ApplicationController
+  def index
+
+    params[:order] = params[:order].presence || 'date'
+    params[:sort_mode] = params[:sort_mode].presence || 'desc'
+
+    @dates = DateToRemember.order(params[:order] => params[:sort_mode])
+    @date = DateToRemember.new
+  end
+
+  def create
+    DateToRemember.create! params.require(:date_to_remember).permit(:date, :title)
+    redirect_to :back
+  end
+end
