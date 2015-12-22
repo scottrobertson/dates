24762434fa4ecf7a40fb4e07af60a5973cedd789
Scott Robertson <scottymeuk@gmail.com>
2015-12-22 00:08:04 +0000

    Nicer dates

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass', '~> 3.3.6'
 gem "table-for"
 gem "haml"
+gem 'time_difference'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/helpers/dates_helper.rb
+++ app/helpers/dates_helper.rb
@@ -1,2 +1,27 @@
 module DatesHelper
+
+  def component_date(date)
+    component = TimeDifference.between(date, Date.today).in_general
+    output = []
+    if component[:years] > 0
+      output << pluralize(component[:years], 'year')
+    end
+
+    if component[:months] > 0
+      output << pluralize(component[:months], 'month')
+    end
+
+    if component[:weeks]> 0
+      output << pluralize(component[:weeks], 'week')
+    end
+
+    if component[:days]> 0
+      output << pluralize(component[:days], 'day')
+    end
+
+    output = output.join(', ')
+
+    "#{output} ago"
+  end
+
 end
