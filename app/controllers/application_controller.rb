8573e25d8cedd41956f6543ecb1185fc74b5ad74
Scott Robertson <scottymeuk@gmail.com>
2017-03-18 20:59:43 +0000

    Setting up basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with name: "scott", password: "1122"
+  http_basic_authenticate_with name: (ENV['USERNAME'] || 'admin'), password: (ENV['PASSWORD'] || 'password')
 end
